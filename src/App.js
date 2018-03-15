eeb198ff06ab5d5092cb4f40f6d1e9c199feb3e4
R Adysurya A <ri7nz.labs@gmail.com>
2018-03-15 17:13:40 +0700

    Update Theme & Replace RSS for my github.io, later

--- src/App.js
+++ src/App.js
@@ -1,114 +1,55 @@
 import React, { Component } from 'react';
+import { BrowserRouter, NavLink } from 'react-router-dom'; 
+import { Switch, Route } from 'react-router'; 
 
-const toText   = (content, limit=0, except=' ...') => {
-    content     = content.split(/<\s*p[^>]*>([^<]*)<\s*\/\s*p\s*>/);
-    content     = content.filter((v) => {
-        return v.length > 100; 
-    });
+import Articles from './Pages/article.js'; 
+import IG from './Pages/instagram.js'; 
 
-    content   = content[0].replace(/<[^>]+>/g, '');
-    return content.substring(0,limit) + except;
-}
-
-const Card     = (props) => {
+const Header = props => {
     return (
-            <div className="card"  alt={props.title} onClick={props.onClick}>
-            <div className="card-img" 
-            style={{backgroundImage: '' }}
-            data-src={props.thumbnail }
-            >
-            </div>
-            <div className="card-body">
-            <h1 className="card-title text-black"> { props.title } </h1>
-            <div className="card-caption">
-            { toText( props.content, 200 ) }
-            </div>
-            </div> 
-            </div>        
-           );
-}
-
-const CardList = props => {
-    return (
-            <div className="cards">
-            {props.items.map( (item,index) => <Card {...item} key={index}  /> )}
-            </div> 
-           );
-}
-
-class App extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            feed    : [],
-            isLoaded: false,
-        };
-    }  
-
-    fetchCache(){
-        let url     = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fwwwid';
-        let cacheKey = url;
-        let cached   = sessionStorage.getItem(cacheKey);
-
-        if (cached !== null){
-            this.setState({
-                feed: JSON.parse(cached),
-                isLoaded: true,
-                isCached: true,
-                isFetched: false,
-            }); 
-            return; 
-        }
-        return fetch(url).then((res) => {
-            if (res.status === 200) {
-                res.clone().text().then(content => {
-                    sessionStorage.setItem(cacheKey, content); 
-                }) 
-            }
-            return res.json();
-
-        }).then((json) => {
-            this.setState({feed: json, isLoaded: true}); 
-            console.log(this.state.feed); 
-        });
-    }
-
-    componentDidMount(){
-        this.fetchCache(); 
-    }
-    Lazy(){
-        [].forEach.call(
-                document.querySelectorAll('.card-img[data-src]'), function(img){
-                    img.style.backgroundImage = `url("${img.getAttribute('data-src')}")`;
-                    img.style.opacity = 100; 
-                });
-    }
-
-    componentDidUpdate(){
-        this.Lazy(); 
-    }
-
-    render() {
-        if(! this.state.isLoaded ){
-            return (
-                    <div className="card"  alt="Loading content ...">
-                    <div className="card-img" >
-                    </div>
-                    <div className="card-body">
-                    <h1 className="card-title bg-blue-dark text-blue-dark">wkwkwkwkwkwkwkkw</h1>
-                    <div className="text-blue-dark bg-blue-dark card-caption">
-                    {'wk'.repeat(170)}
-                    </div>
-                    </div> 
-                    </div>        
-                   );
-        }
-        return (
-                <div className="article">
-                <CardList items={this.state.feed.items} />
+            <div className="page-title flex-col">
+               <h1>R I 7 N Z</h1>
+               <div className="nav">
+               <ul className="inline-flex list-reset">
+                <li><NavLink to="/">Article</NavLink></li>
+                <li><NavLink to="/instagram">Instagram</NavLink></li>
+                <li><NavLink to="/projects">Projects</NavLink></li>
+                </ul>
                 </div>
-               );
+               <p align="center" className="font-light w-md text-sm"> 
+                    Geek, Living the day by write code with linux  and listen Rock & Metal Music, Drinking Coffe, and Sacred Geometry Enthusias.
+               </p>  
+            </div>
+    );
+}
+
+const Article = props => {
+    return (
+        <div>
+        <Header/>
+        <Articles/>
+        </div>
+    );
+}
+const Instagram = props => {
+    return (
+        <div>
+        <Header/>
+        <IG/>
+        </div>
+    );
+}
+class App extends Component {
+    render(){
+        return (
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/" exact component={Article} />
+                        <Route path="/instagram" exact component={Instagram} />
+                    </Switch>
+                </BrowserRouter>
+        );
     }
 }
 
-export default App;
+export default App; 
--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routers from './App';
+import App from './App';
 import './style.css';
 import registerServiceWorker from './registerServiceWorker';
+import 'typeface-roboto'; 
 
-const Header = props => {
-    return (
-            <div className="page-title flex-col">
-               <h1>#Ri7NZ | WWWID Challenge</h1>
-               <h3 className="font-light"> <a href="https://medium.com/wwwid/tantangan-web-developer-untuk-membuat-aplikasi-web-bisa-digunakan-kurang-dari-5-detik-70bb7431741d"> Read : Tantangan Web Developer Untuk Membuat Aplikasi Web Bisa Digunakan Kurang Dari 5 Detik </a></h3>  
-            </div>
-    );
-}
 ReactDOM.render(
         <div>
-            <Header />
-            <Routers />
+            <App />
         </div>
 , document.getElementById('r00t'));
 registerServiceWorker();
